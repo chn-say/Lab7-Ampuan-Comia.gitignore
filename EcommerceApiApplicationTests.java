49237a129e7e57918b63168d37f82efe56637c8b
chn-say <ampuanrichellec@gmail.com>
2026-04-24 22:26:09 +0800

    Add files via upload

---
+++ EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws101.Ampuan.Comia.EcommerceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
